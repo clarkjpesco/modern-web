8d32632010afcbceb6072d8159eeb1187d14cf44
Clark Joy Pesco <clarkjoypesco@gmail.com>
2023-04-06 20:54:13 +0800

    few changes on config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: "/modern-web",
+  assetPrefix: "/modern-web",
   plugins: [
     `gatsby-plugin-image`,
     {
